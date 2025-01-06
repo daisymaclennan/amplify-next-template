545abe880aace544fa99e3710037e8f31451c735
Daisy <daisylouisemaclennan@gmail.com>
2025-01-06 17:56:27 +0000

    added authenticator

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./app.css";
 
+import "@aws-amplify/ui-react/styles.css";
+import Authenticator from "@/src/components/Authenticator";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Authenticator>{children}</Authenticator>
+      </body>
     </html>
   );
 }
---
+++ src/components/Authenticator.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import { Authenticator as AmplifyAuthenticator } from "@aws-amplify/ui-react";
+
+export default AmplifyAuthenticator;
